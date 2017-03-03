940d893309b8fb61ed3c3510571f21da0ea55652
shamaleenarkhede <shamaleenarkhede@gmail.com>
2017-03-03 17:28:38 -0500

    Add "Scorecard UI"

---
+++ Camarillo Tennis Club/Controllers/ScoreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Camarillo_Tennis_Club.Controllers
+{
+    public class ScoreController : Controller
+    {
+        // GET: Score
+        public ActionResult Scores()
+        {
+            return View();
+        }
+    }
+}
